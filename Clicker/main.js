40a64917255060b0d76ceb5cc63664ffcb7ad58e
dzhigit1 <75542241+dzhigit1@users.noreply.github.com>
2022-08-31 00:39:11 -0400

    Update main.js

--- Clicker/main.js
+++ Clicker/main.js
@@ -2,19 +2,19 @@ if (localStorage.getItem("counts"))
     let counts = localStorage.getItem("counts");
 else
     let counts = {
-        num: 0,
-        cursorCount: 0,
-        grandmaCount: 0,
-        farmCount: 0,
-        mineCount: 0
+        cookies: 0,
+        cursors: 0,
+        grandmas: 0,
+        farms: 0,
+        mines: 0
     };
 
 let h1 = document.getElementById('inbank');
 let cookie = document.getElementById('cookie');
 
 cookie.onclick = function() {
-    num++;
-    h1.innerText = Math.floor(num);
+    counts.cookies++;
+    h1.innerText = Math.floor(counts.cookies);
 }
 
 // Buildings
@@ -29,10 +29,10 @@ let grandmaprice = 100;
 let farmprice = 1100;
 let mineprice = 12000;
 
-cursor.setAttribute("count", cursorcount);
-grandma.setAttribute("count", grandmacount);
-farm.setAttribute("count", farmcount);
-mine.setAttribute("count", minecount);
+cursor.setAttribute("count", counts.cursors);
+grandma.setAttribute("count", counts.grandmas);
+farm.setAttribute("count", counts.farms);
+mine.setAttribute("count", counts.mines);
 
 cursor.setAttribute("price", cursorprice);
 grandma.setAttribute("price", grandmaprice);
@@ -40,47 +40,47 @@ farm.setAttribute("price", farmprice);
 mine.setAttribute("price", mineprice);
 
 cursor.onclick = function() {
-    if(num >= cursorprice) {
-        cursorcount++;
-        num -= cursorprice;
-        cursorprice = 15 * (1.15 ** cursorcount);
+    if(counts.cookies >= cursorprice) {
+        counts.cursors++;
+        counts.cookies -= cursorprice;
+        cursorprice = 15 * (1.15 ** counts.cursors);
     }
-    cursor.setAttribute("count", cursorcount);
+    cursor.setAttribute("count", counts.cursors);
     cursor.setAttribute("price", Math.ceil(cursorprice));
 }
 grandma.onclick = function() {
-    if(num >= grandmaprice) {
-        grandmacount++;
-        num -= grandmaprice;
-        grandmaprice = 100 * (1.15 ** grandmacount);
+    if(counts.cookies >= grandmaprice) {
+        counts.grandmas++;
+        counts.cookies -= grandmaprice;
+        grandmaprice = 100 * (1.15 ** counts.grandmas);
     }
-    grandma.setAttribute("count", grandmacount);
+    grandma.setAttribute("count", counts.grandmas);
     grandma.setAttribute("price", Math.ceil(grandmaprice));
 }
 farm.onclick = function() {
-    if(num >= farmprice) {
-        farmcount++;
-        num -= farmprice;
-        farmprice = 1100 * (1.15 ** farmcount);
+    if(counts.cookies >= farmprice) {
+        counts.farms++;
+        counts.cookies -= farmprice;
+        farmprice = 1100 * (1.15 ** counts.farms);
     }
-    farm.setAttribute("count", farmcount);
+    farm.setAttribute("count", counts.farms);
     farm.setAttribute("price", Math.ceil(farmprice));
 }
 mine.onclick = function() {
     if(num >= mineprice) {
-        minecount++;
-        num -= mineprice;
-        mineprice = 12000 * (1.15 ** minecount);
+        counts.mines++;
+        counts.cookies -= mineprice;
+        mineprice = 12000 * (1.15 ** counts.mines);
     }
-    mine.setAttribute("count", minecount);
+    mine.setAttribute("count", counts.mines);
     mine.setAttribute("price", Math.ceil(mineprice));
 }
 
 // CPS
 
 setInterval(function () {
-    num += (cursorcount * .1) + (grandmacount) + (farmcount * 8) + (minecount * 47);
-    h1.innerText = Math.floor(num);
+    num += (counts.cursors * .1) + (counts.grandmas) + (counts.farms * 8) + (counts.mines * 47);
+    h1.innerText = Math.floor(counts.cookies);
 }, 1000);
 
 setInterval(function () {
